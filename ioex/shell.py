31347eca9c635b7f537f65c5a0301dd96b6b7bfd
Fabian Peter Hammerle <fabian.hammerle@gmail.com>
2016-02-26 13:56:04 +0100

    added module 'shell'

---
+++ ioex/shell.py
@@ -0,0 +1,14 @@
+# http://misc.flogisoft.com/bash/tip_colors_and_formatting
+
+class Formatting:
+    bold = '\033[1m'
+    dim = '\033[2m'
+    reset_bold = '\033[21m'
+    reset_dim = '\033[22m'
+    reset = ''.join([reset_bold, reset_dim])
+
+class TextColor:
+    default = '\033[39m'
+    red = '\033[31m'
+    green = '\033[32m'
+    blue = '\033[34m'
